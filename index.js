70bf42d6a816c43a73b9796740e770d3c73aea61
Caius Brindescu <brindesc@oregonstate.edu>
2017-06-17 21:15:23 -0700

    Basic app

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const passport = require('passport')
+const app = express()
+
+//app.use(require('serve-static')(__dirname + '/../../public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
