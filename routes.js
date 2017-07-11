feaa904033dd41052d339b59ba5e39cab09ecd78
Caius Brindescu <brindesc@oregonstate.edu>
2017-07-11 16:06:02 -0700

    Formatting

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Account = require('./models/account');
 var Note = require("./models/note");
 
-module.exports = function(passport) {
+module.exports = function (passport) {
   var router = express.Router();
 
   router.get('/', function (req, res, next) {
@@ -11,20 +11,20 @@ module.exports = function(passport) {
     else
       res.render('home', {'title': 'Basic Notes'});
   });
-  
+
   router.get('/notes', function(req, res, next) {
     Note.find({owner: req.user.id}).exec(function(err, notes) {
       res.render('notes', {notes: notes});
     });
   });
-  
+
   router.post('/notes', function(req, res, next) {
     var note = new Note({title: req.body.title, content: req.body.content, owner: req.user.id});
     note.save(function(err, note) {
         res.redirect('/notes');
       });
   });
-  
+
   router.get('/notes/:id', function(req, res, next) {
     Note.findOne({_id: req.params.id}, function(err, note) {
       if (err) {
@@ -33,7 +33,7 @@ module.exports = function(passport) {
       res.render('edit_note', {note: note});
     });
   });
-  
+
   router.post('/notes/:id', function(req, res, next) {
     Note.findOneAndUpdate({_id: req.params.id}, req.body, function(err, note) {
       if (err)
@@ -41,23 +41,23 @@ module.exports = function(passport) {
       res.redirect('/notes');
     });
   });
-  
-  
+
+
   router.get('/new', function(req, res, next) {
     res.render('edit_note');
   });
-  
+
   router.get('/about', function(req, res, next) {
     res.render('about', {'title': 'About'});
   });
-  
+
   router.get('/account', function(req, res, next) {
     if (req.isAuthenticated)
       res.redirect('/account/' + req.user.username);
     else
       res.redirect('/login');
   });
-  
+
   router.get('/account/:id', function(req, res) {
     Account.findOne({username: req.params.id}, function(err, user) {
         if (err)
@@ -70,37 +70,37 @@ module.exports = function(passport) {
         }
     });
   });
-  
+
   router.get('/login', function(req, res, next) {
     res.render('login', {'title': 'Log In'});
   });
-  
+
   router.post('/login',
     passport.authenticate('local', { successRedirect: '/',
                                      failureRedirect: '/login'})
   );
-  
+
   router.get('/register', function(req, res, next) {
     res.render('register', {'title': "Register new account"});
   })
-  
+
   router.post('/register', function(req, res, next) {
     Account.register(new Account({username: req.body.username, email: req.body.email}), req.body.password, function(err, account) {
       if (err) {
         console.log(err.toString())
         return res.render('register', {account: account, error: err});
       }
-  
+
       passport.authenticate('local')(req, res, function() {
         res.redirect('/');
       });
     });
   });
-  
+
   router.get('/logout', function(req, res, next) {
     req.logout();
     res.redirect('/');
   });
-  
+
   return router;
 }
